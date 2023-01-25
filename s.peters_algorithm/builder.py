27ee38107cfb2317c656984716134e74d32ee8c3
julblanke <j_blan06@wwu.de>
2023-01-25 12:28:28 +0100

    ref: arg parser is moved to "_get_user_args"-function

--- s.peters_algorithm/builder.py
+++ s.peters_algorithm/builder.py
@@ -6,28 +6,15 @@ from parser import Parser
 
 class Builder:
 
-    @staticmethod
-    def run():
+    def run(self):
         """ run s.peters_algorithm to create xml-file containing objects specified in config file.
             objects are given as xml by user
         """
 
-        # read args set by user
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--config_path", help="Specify folder where yaml file is located",
-                            default=None)
-        parser.add_argument("--xml_dir", help="Specify folder where all xml files are located",
-                            default=None)
-        args = parser.parse_args()
+        # assign user args to params
+        args = self._get_user_args()
         config_path = args.config_path
         xml_dir = args.xml_dir
-
-        if xml_dir is None:
-            xml_dir = "./examples/xml_objects"
-            warnings.warn("xml directory not specified; running with default directory in examples")
-        if config_path is None:
-            config_path = "./examples/config_files/simple-config.yml"
-            warnings.warn("config path not specified; running with default directory in examples")
 
         # call Parser class to read given config file and get mjcf-objects in given directory
         parser = Parser(config_path=config_path, xml_dir=xml_dir)
@@ -41,10 +28,30 @@ class Builder:
 
         # ToDo: add mujoco-object to areas; objects from parser
 
+    def _get_user_args(self):
+        """ read args set by user; if none are given, args are set to files and directories in "examples" """
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--config_path", help="Specify folder where yaml file is located",
+                            default=None)
+        parser.add_argument("--xml_dir", help="Specify folder where all xml files are located",
+                            default=None)
+        args = parser.parse_args()
+
+        if args.xml_dir is None:
+            args.xml_dir = "./examples/xml_objects"
+            warnings.warn("xml directory not specified; running with default directory in examples")
+        if args.config_path is None:
+            args.config_path = "./examples/config_files/simple-config.yml"
+            warnings.warn("config path not specified; running with default directory in examples")
+
+        return args
+
     def _to_xml(self):
         """ combine MJCFs to single xml-file """
         pass
 
 
 if __name__ == "__main__":
-    Builder.run()
+    builder = Builder()
+    builder.run()
