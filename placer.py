c2aff51194bc3035ab949e8fe53629afbd626120
Jacob Dudek <jadudek@uni-osnabrueck.de>
2023-01-25 18:08:47 +0100

    feature: adapted type hints, removed area and env from init, added validator to add function

--- placer.py
+++ placer.py
@@ -1,18 +1,20 @@
 from abc import ABC, abstractmethod
 from dm_control import mjcf
-import Environment
+import Validator
 import Site
 
 
 class Placer(ABC):
     @abstractmethod
-    def __init__(self, environment: Environment):
-        self.environment = environment
+    def __init__(self):
+        pass
 
     @abstractmethod
-    def add(self, site: Site, mujoco_object: mjcf):
+    def add(
+        self, site: Site, mujoco_object: mjcf.RootElement, validator: list[Validator]
+    ):
         site.add(mujoco_object)
 
     @abstractmethod
-    def remove(self, site: Site, mujoco_object: mjcf):
+    def remove(self, site: Site, mujoco_object: mjcf.RootElement):
         site.remove(mujoco_object)
