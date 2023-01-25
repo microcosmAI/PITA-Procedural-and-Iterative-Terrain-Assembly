09a521005a1404379e0b60ae951872d80d6756aa
julblanke <j_blan06@wwu.de>
2023-01-25 17:32:05 +0100

    feat: reduced parser functionality; now simply takes a xml and returns mjcf

---
+++ s.peters_algorithm/utils/parser.py
@@ -0,0 +1,24 @@
+import os
+
+from dm_control import mjcf
+
+
+class Parser:
+    """ class to parse xml-file in given path to mujoco's MJCF-object """
+
+    @staticmethod
+    def get_mjcf(*, xml_path: str = "../examples/humanoid.xml"):
+        """ parses xml-file to MJCF-object
+
+        Returns:
+        -------
+        mjcf_: mjcf-object
+            mjcf object of given xml-file
+        """
+        if xml_path is None:
+            raise ValueError("No xml file provided")
+        if not os.path.isfile(xml_path):
+            raise ValueError("Could not find xml path provided")
+
+        mjcf_ = mjcf.from_path(xml_path)
+        return mjcf_
