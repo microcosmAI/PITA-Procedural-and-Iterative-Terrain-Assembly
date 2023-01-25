f83ee81f440af2e2893c6166d467ecbf0bed381d
Gerrit Bartels <64156238+GerritBartels@users.noreply.github.com>
2023-01-25 18:51:22 +0100

    feature: added add and remove functions

--- environment.py
+++ environment.py
@@ -1,7 +1,8 @@
 from dm_control import mjcf
+import Site
 
 
-class Environment:
+class Environment(Site):
     """Class that represents the entire environment"""
 
     def __init__(self, size: tuple[float, float, float]):
@@ -11,8 +12,28 @@ class Environment:
         Parameters:
             size (tuple): Tuple defining the size of the entire environment
         """
+        super(Environment, self).__init__()
+
         self.size = size
         self.mjcf_model = mjcf.RootElement()
         self.mjcf_model.worldbody.add(
             "geom", name="base_plane", type="plane", size=size
         )
+
+    def add(self, mujoco_object: mjcf.RootElement):
+        """
+        Adds a mujoco object
+
+        Parameters:
+            mujoco_object (mjcf): Tuple defining the size of the entire environment
+        """
+        self.mjcf_model.attach(mujoco_object)
+
+    def remove(self, mujoco_object: mjcf.RootElement):
+        """
+        Removes a given mujoco object
+
+        Parameters:
+            mujoco_object (mjcf): Tuple defining the size of the entire environment
+        """
+        mujoco_object.detach()
