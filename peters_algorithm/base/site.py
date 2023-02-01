3ba38943896f646bbceb1941ee887c8a8fd8fb00
julblanke <j_blan06@wwu.de>
2023-02-01 15:00:02 +0100

    feature: added site class

---
+++ peters_algorithm/base/site.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+class Site(ABC):
+    """  abstract class for environment and area functionality
+    """
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def add(self):
+        """ add functionality to place objects in child classes """
+        pass
+
+    @abstractmethod
+    def remove(self):
+        """ remove functionality to place objects in child classes """
+        pass
+
+    @abstractmethod
+    def get_mjcf(self):
+        """ functionality to retrieve mjcf object of finished design of child classes """
+        pass
