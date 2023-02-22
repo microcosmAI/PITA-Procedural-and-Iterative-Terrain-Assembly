398a9d77ea52613eb7fb4dcf5b43fdab52637a43
julblanke <j_blan06@wwu.de>
2023-02-22 15:35:40 +0100

    feature: added "isActive" parameter to handle new config requirements

--- peters_algorithm/base/border_placer.py
+++ peters_algorithm/base/border_placer.py
@@ -21,6 +21,7 @@ class BorderPlacer(Placer):
         environment: Environment,
         mujoco_object_blueprint: MujocoObject,
         amount: int = 4,
+        isActive: bool = False
     ):
         """Adds the borders around the environment
 
@@ -28,48 +29,49 @@ class BorderPlacer(Placer):
             environment (Environment): Environment class instance
             mujoco_object_blueprint (MujocoObject): Blueprint of to-be-placed mujoco object
             amount (int): Number of to-be-placed borders
-            height (int): Height of to-be-placed borders
+            isActive (bool): True if border is added to environment, else False
 
         Returns:
             mjcf_model (mjcf): An empty environment with borders around it
         """
-        size = environment.size
+        if isActive:
+            size = environment.size
 
-        blueprint_x, blueprint_y, blueprint_z = (
-            mujoco_object_blueprint.mjcf_obj.worldbody.body[0].geom[0].size
-        )
+            blueprint_x, blueprint_y, blueprint_z = (
+                mujoco_object_blueprint.mjcf_obj.worldbody.body[0].geom[0].size
+            )
 
-        # Calculate border coordinates, add/sub width/breadth of the object to remove overlap with the plane
-        top_middle = (0, size[1] + blueprint_y)
-        bottom_middle = (0, -size[1] - blueprint_y)
-        right_middle = (size[0] + blueprint_x, 0)
-        left_middle = (-size[0] - blueprint_x, 0)
+            # Calculate border coordinates, add/sub width/breadth of the object to remove overlap with the plane
+            top_middle = (0, size[1] + blueprint_y)
+            bottom_middle = (0, -size[1] - blueprint_y)
+            right_middle = (size[0] + blueprint_x, 0)
+            left_middle = (-size[0] - blueprint_x, 0)
 
-        # Coordinates are given in halfs already
-        coords = (top_middle, bottom_middle, right_middle, left_middle)
+            # Coordinates are given in halfs already
+            coords = (top_middle, bottom_middle, right_middle, left_middle)
 
-        borders = [self._copy(mujoco_object_blueprint) for _ in range(amount)]
+            borders = [self._copy(mujoco_object_blueprint) for _ in range(amount)]
 
-        for idx, border in enumerate(borders):
-            border_body = border.mjcf_obj.worldbody.body[0]
+            for idx, border in enumerate(borders):
+                border_body = border.mjcf_obj.worldbody.body[0]
 
-            # Enlarge border on x-axis
-            if coords[idx][0] == 0:
-                border_body.geom[0].size[0] = size[0]
+                # Enlarge border on x-axis
+                if coords[idx][0] == 0:
+                    border_body.geom[0].size[0] = size[0]
 
-                border_body.pos[0] = coords[idx][0]
-                border_body.pos[1] = coords[idx][1]
-                border_body.pos[2] = blueprint_z
+                    border_body.pos[0] = coords[idx][0]
+                    border_body.pos[1] = coords[idx][1]
+                    border_body.pos[2] = blueprint_z
 
-            # Enlarge border on y-axis
-            else:
-                border_body.geom[0].size[1] = size[1]
+                # Enlarge border on y-axis
+                else:
+                    border_body.geom[0].size[1] = size[1]
 
-                border_body.pos[0] = coords[idx][0]
-                border_body.pos[1] = coords[idx][1]
-                border_body.pos[2] = blueprint_z
+                    border_body.pos[0] = coords[idx][0]
+                    border_body.pos[1] = coords[idx][1]
+                    border_body.pos[2] = blueprint_z
 
-            environment.add(mujoco_object=border)
+                environment.add(mujoco_object=border)
 
     def remove(self, *, site: Site, mujoco_object: MujocoObject):
         """Remove function, currently not implemented/needed"""
