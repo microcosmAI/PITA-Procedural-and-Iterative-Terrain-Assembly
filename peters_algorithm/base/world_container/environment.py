4ad48485394a5fe77f947f157ababbca75fe0ef3
GerritBartels <gerrit.bartels@uos.de>
2023-05-11 19:00:58 +0200

    feature: added pretty mode for demo purposes

--- peters_algorithm/base/world_container/environment.py
+++ peters_algorithm/base/world_container/environment.py
@@ -7,19 +7,35 @@ from peters_algorithm.base.world_container.abstract_container import AbstractCon
 class Environment(AbstractContainer):
     """Class that represents the entire environment"""
 
-    def __init__(self, *, size: tuple[float, float, float], name: str = "Environment"):
+    def __init__(self, *, size: tuple[float, float, float], name: str = "Environment", pretty_mode: bool = False):
         """Initializes the environment class
 
         Parameters:
             size (tuple): Tuple defining the size of the entire environment (length, width, height)
             name (str): Name of the environment
+            pretty_style (bool): If true, the environment will be created with a pretty style
         """
         self._size = size
         self._name = name
         self._mjcf_model = mjcf.RootElement()
-        self._mjcf_model.worldbody.add(
-            "geom", name="base_plane", type="plane", size=size
-        )
+
+        if pretty_mode:
+            self._mjcf_model.asset.add(
+                "texture", type="skybox", builtin="gradient", rgb1=".3 .5 .7", rgb2="0 0 0", width="512", height="512"
+            )
+            self._mjcf_model.asset.add(
+                "texture", name="grid", type="2d", builtin="checker", rgb1=".1 .2 .3", rgb2=".2 .3 .4", width="300", height="300", mark="edge", markrgb=".2 .3 .4"
+            )
+            self._mjcf_model.asset.add(
+                "material", name="grid", texture="grid", texrepeat="1 1", texuniform="true", reflectance=".2"
+            )
+            self._mjcf_model.worldbody.add(
+                "geom", name="base_plane", type="plane", size=size, material="grid"
+            )
+        else:
+            self._mjcf_model.worldbody.add(
+                "geom", name="base_plane", type="plane", size=size
+            )
         self._mujoco_objects = {}
 
     @property
