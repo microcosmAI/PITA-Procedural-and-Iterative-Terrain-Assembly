1464fff7e7dc359b6505574be5aa8ad34e26cd32
Peter Keffer <peter@keffer.de>
2023-05-16 18:37:35 +0200

    bugfix: fixed wrong comma

--- peters_algorithm/base/assembler.py
+++ peters_algorithm/base/assembler.py
@@ -6,7 +6,7 @@ from peters_algorithm.base.asset_placement.validator import Validator
 from peters_algorithm.base.world_container.environment import Environment
 from peters_algorithm.base.asset_parsing.mujoco_loader import MujocoLoader
 from peters_algorithm.base.asset_placement.fixed_placer import FixedPlacer
-from peters_algorithm.base.asset_placement.random_placer import RandomPlacer, Placer2DDistribution,
+from peters_algorithm.base.asset_placement.random_placer import RandomPlacer, Placer2DDistribution
 from peters_algorithm.base.asset_placement.border_placer import BorderPlacer
 from peters_algorithm.base.asset_placement.boundary_rule import BoundaryRule
 from peters_algorithm.base.asset_placement.min_distance_rule import MinDistanceRule
