cc2a45eece6265639a1fee1d1366c2d4bd99e63c
Jacob Dudek <jadudek@uni-osnabrueck.de>
2023-05-24 18:17:00 +0200

    bugfix: let area use same mjcf_model as environment + added boundary parameter with getter and setter

--- peters_algorithm/base/world_container/area.py
+++ peters_algorithm/base/world_container/area.py
@@ -1,5 +1,6 @@
 from dm_control import mjcf
 
+from peters_algorithm.base.world_container.environment import Environment
 from peters_algorithm.base.asset_parsing.mujoco_object import MujocoObject
 from peters_algorithm.base.world_container.abstract_container import AbstractContainer
 
@@ -7,20 +8,22 @@ from peters_algorithm.base.world_container.abstract_container import AbstractCon
 class Area(AbstractContainer):
     """Area object class"""
 
-    def __init__(self, *, name: str, size: tuple[float, float, float]):
+    def __init__(self, *, name: str, size: tuple[float, float, float], environment: Environment, boundary: tuple):
         """Initialize area
 
         Parameters:
             name (str): Name of area
             size (tuple): Size of area
-            mjcf_model (mjcf): Mjcf model of area
             mujoco_objects (dict): Dictionary of mujoco objects in area
+            environment (Environment): Environment class instance
+            boundary (tuple): Boundary of the area that constrains object placement
         """
         self._name = name
         self._size = size
-        self._mjcf_model = mjcf.RootElement(model=self.name)
+        self._mjcf_model = environment.mjcf_model
         # self._mjcf_model.worldbody.add("geom", name="base_plane_area", type="plane", size=self._size)
         self._mujoco_objects = {}
+        self._boundary = boundary
 
     def add(self, *, mujoco_object: MujocoObject):
         """Add object to area-mjcf and mujoco-object dictionary of area. Also sets name of object to the one given by mujoco
@@ -32,12 +35,11 @@ class Area(AbstractContainer):
         attachement_frame = self._mjcf_model.attach(mujoco_object.mjcf_obj)
         # By calling all_children() on the attachement frame, we can access their uniqe identifier
         mujoco_object.xml_id = (
-            self._name + "/" + attachement_frame.all_children()[0].full_identifier
+            attachement_frame.all_children()[0].full_identifier
         )
 
         # Check for free joints (either <joint type="free"/> or <freejoint/> but always as a direct child)
         # If present, remove it and add it again one level above
-        # TODO: as of now does not work properly once the whole area gets attached to the env
         joint_list = attachement_frame.all_children()[0].find_all("joint", immediate_children_only=True)
         if joint_list:
             if joint_list[0].tag == "freejoint" or joint_list[0].type == "free":
@@ -93,3 +95,17 @@ class Area(AbstractContainer):
             mujoco_objects (dict): Dictionary of mujoco objects in area
         """
         return self._mujoco_objects
+
+    @property
+    def boundary(self):
+        """Get area boundary
+
+        Returns:
+            boundary (tuple): Tuple with the area boundary
+        """
+        return self._boundary
+
+    @boundary.setter
+    def boundary(self, boundary: tuple):
+        """Set boundary"""
+        self.boundary = boundary
