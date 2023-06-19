101192db5366f750d2bd4a93eb235ccb50d9c70f
Jacob Dudek <jadudek@uni-osnabrueck.de>
2023-06-19 14:05:35 +0200

    misc: updated multivariate_unfiform_distribution.py + feature: added placeholder abstract placer distribtion

--- peters_algorithm/utils/multivariate_uniform_distribution.py
+++ peters_algorithm/utils/multivariate_uniform_distribution.py
@@ -5,19 +5,18 @@ class MultivariateUniform:
     """Generates a multivariate uniform distribution.
 
     Parameters:
-        ranges (list[tuple]): Each tuple defines the range (low, high) for one dimension.
+        ranges (list[tuple]): Each tuple defines the range (low, high) for one dimension
 
     Returns:
-        (tuple[np.ndarray, np.ndarray]): An array of shape (num_samples, len(ranges)), where each row is a sample from the multivariate uniform distribution.
+        x, y (tuple[float, float]): Sampled x and y coordinates
     """
 
     def __call__(self, ranges: list[tuple]) -> tuple[np.ndarray, np.ndarray]:
         # Generate samples for each dimension
         samples = [
-            np.random.uniform(low=low, high=high, size=1) for low, high in ranges
+            np.round(np.random.uniform(low=low, high=high, size=1)[0], 4) for low, high in ranges
         ]
 
-        # Combine the samples for each dimension
-        multivariate_samples = np.column_stack(samples)
+        x, y = samples
 
-        return multivariate_samples[0][0], multivariate_samples[0][1]
+        return x, y
