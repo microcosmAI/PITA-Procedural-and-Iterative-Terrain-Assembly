1a26bbfd795eca9905bc154922de7e6203d62d8f
julblanke <j_blan06@wwu.de>
2023-07-04 17:56:29 +0200

    bugfix: fixed naming conflicts of duplicates in ymls when dealing with fixed and random placement settings of same object feat: added obj_class parameter

--- peters_algorithm/base/asset_parsing/mujoco_loader.py
+++ peters_algorithm/base/asset_parsing/mujoco_loader.py
@@ -59,13 +59,21 @@ class MujocoLoader:
         """
         mujoco_dict = {}
         for obj, params in obj_dict.items():
-            obj_xml_path = os.path.join(self.xml_dir, obj + ".xml")
+            xml_name = None
+            for entry in params:
+                if entry.get("xml_name") is not None:
+                    xml_name = entry["xml_name"]
+            obj_xml_path = os.path.join(self.xml_dir, xml_name)
             mjcf = Parser.get_mjcf(xml_path=obj_xml_path)
+            asset_name = xml_name.split(".xml")[0]
+            mjcf.find("body", asset_name.lower()).name = obj.lower()
+            mjcf.root.model = obj.lower()
             obj_type, attachable, tags = self._read_params(params)
             mujoco_obj = MujocoObject(
                 name=obj,
                 xml_id="",
                 mjcf_obj=mjcf,
+                obj_class=asset_name,
                 obj_type=obj_type,
                 attachable=attachable,
                 color=None,
