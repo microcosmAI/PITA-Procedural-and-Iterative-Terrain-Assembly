4e5a9e81f557d32fb5f845ce612740d36093de3d
julblanke <j_blan06@wwu.de>
2023-12-13 15:56:45 +0100

    misc: added check for mismatch of amount and coordinates for fixed objects

--- pita_algorithm/base/asset_placement/fixed_placer.py
+++ pita_algorithm/base/asset_placement/fixed_placer.py
@@ -53,6 +53,13 @@ class FixedPlacer(AbstractPlacer):
             mujoco_objects_blueprints (Union[dict, None]): Dictionary of all objects as mujoco-objects
         """
         logger = logging.getLogger()
+        # Check if amount of objects and given coordinates is correct
+        if not amount == len(coordinates):
+            logger.error(f"Mismatch between given object amount and coordinates for object"
+                         f" '{mujoco_object_rule_blueprint.name}'.")
+            raise ValueError(f"Mismatch between given object amount and coordinates for object"
+                             f" '{mujoco_object_rule_blueprint.name}'.")
+
         # Get colors rgba
         if not color_groups is None:
             if max(color_groups) > amount:
