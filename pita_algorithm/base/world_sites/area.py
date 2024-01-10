b5c2c5f107b0a1639175aa15dfcaec0d3358dcd5
julblanke <j_blan06@wwu.de>
2024-01-10 16:37:46 +0100

    refactor: minor changes

--- pita_algorithm/base/world_sites/area.py
+++ pita_algorithm/base/world_sites/area.py
@@ -1,10 +1,7 @@
-import numpy as np
 from dm_control import mjcf
-
 from pita_algorithm.base.world_sites.environment import Environment
 from pita_algorithm.base.world_sites.abstract_site import AbstractSite
 from pita_algorithm.base.asset_parsing.mujoco_object import MujocoObject
-from pita_algorithm.utils.general_utils import Utils
 
 
 class Area(AbstractSite):
@@ -24,7 +21,6 @@ class Area(AbstractSite):
         Parameters:
             name (str): Name of the area
             size (tuple): Size of the area
-            mujoco_objects (dict): Dictionary of mujoco objects in the area
             environment (Environment): Environment class instance
             boundary (tuple): Boundary of the area that constrains object placement
         """
@@ -45,27 +41,27 @@ class Area(AbstractSite):
         # Offset the coordinates to the boundaries of the area
         # mujoco_object.position = Utils.offset_coordinates_to_boundaries(mujoco_object.position) # TODO check if it mighrt be better to do this in the mujoco_object class
 
-        # The attach() method returns the attachement frame
+        # The attach() method returns the attachment frame
         # i.e., a body with the attached mujoco object
-        attachement_frame = self._mjcf_model.attach(mujoco_object.mjcf_obj)
-        # By calling all_children() on the attachement frame, we can access their uniqe identifier
-        mujoco_object.xml_id = attachement_frame.all_children()[0].full_identifier
+        attachment_frame = self._mjcf_model.attach(mujoco_object.mjcf_obj)
+        # By calling all_children() on the attachment frame, we can access their unique identifier
+        mujoco_object.xml_id = attachment_frame.all_children()[0].full_identifier
 
         # Check for free joints (<joint type="free"/> or <freejoint/> but always as a direct child)
         # If present, remove it and add it again one level above
-        joint_list = attachement_frame.all_children()[0].find_all(
+        joint_list = attachment_frame.all_children()[0].find_all(
             "joint", immediate_children_only=True
         )
         if joint_list:
             if joint_list[0].tag == "freejoint" or joint_list[0].type == "free":
                 joint_attribute_dict = joint_list[0].get_attributes()
                 joint_attribute_dict.pop("type", None)  # pop type key if present
-                attachement_frame.add("joint", type="free", **joint_attribute_dict)
+                attachment_frame.add("joint", type="free", **joint_attribute_dict)
                 joint_list[0].remove()
 
-                # Fix rotation bug, i.e., move euler value into the parent body (attachement_frame) and reset it in the mujoco_object
+                # Fix rotation bug, i.e., move euler value into the parent body (attachment_frame) and reset it in the mujoco_object
                 # For the environment dynamics to work properly (adding the agent's rotation to qvel would otherwise not be possible)
-                attachement_frame.euler = mujoco_object.rotation
+                attachment_frame.euler = mujoco_object.rotation
                 mujoco_object.rotation = (0.0, 0.0, 0.0)
 
         self._mujoco_objects[mujoco_object.xml_id] = mujoco_object
